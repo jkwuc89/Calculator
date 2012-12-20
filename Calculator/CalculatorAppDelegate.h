551c629fe71df2033a24afacb85bcc5b59e861b2
Keith Wedinger <jkwuc89@gmail.com>
2012-12-20 14:53:00 -0500

    Initial Commit

---
+++ Calculator/CalculatorAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CalculatorAppDelegate.h
+//  Calculator
+//
+//  Created by Keith Wedinger on 12/20/12.
+//  Copyright (c) 2012 Keith Wedinger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Calculator/CalculatorViewController.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorViewController.h
+//  Calculator
+//
+//  Created by Keith Wedinger on 12/20/12.
+//  Copyright (c) 2012 Keith Wedinger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorViewController : UIViewController
+
+@end
